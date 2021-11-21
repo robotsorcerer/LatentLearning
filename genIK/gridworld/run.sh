9d0b2a3077dd725dae43e8760b2b7eec4c42337f
Riashat <riashat.islam.93@gmail.com>
2021-11-21 11:50:20 -0500

    major updates

--- genIK/gridworld/run.sh
+++ genIK/gridworld/run.sh
@@ -14,8 +14,135 @@
 python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_final_check --exo_noise --corr_noise 0.1 --use_rgb
 
 
+
+
+
 python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
 python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new_analysis --exo_noise  --corr_noise 0.9 --use_rgb
 python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
+
+
+
+python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new_analysis --exo_noise --corr_noise 0.9
+python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new_analysis --exo_noise  --corr_noise 0.9
+python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new_analysis --exo_noise --corr_noise 0.9
+
+
+
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new_analysis --exo_noise  --corr_noise 0.9 --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
+
+
+python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
+python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
+python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 --use_rgb
+
+
+
+python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 
+python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 
+python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 60 --folder ./results_new_analysis --exo_noise --corr_noise 0.9 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_corr_noise_0p9 --exo_noise --corr_noise 0.9 --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_corr_noise_0p9 --exo_noise --corr_noise 0.9 --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_corr_noise_0p9 --exo_noise --corr_noise 0.9 --use_rgb
+
+
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_corr_noise_0p5 --exo_noise --use_rgb --corr_noise 0.5
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_corr_noise_0p5 --exo_noise --use_rgb --corr_noise 0.5
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_corr_noise_0p5 --exo_noise --corr_noise 0.5 --use_rgb
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_corr_noise_0p5 --exo_noise --corr_noise 0.5 --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_corr_noise_0p5 --exo_noise --corr_noise 0.5 --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_corr_noise_0p5 --exo_noise --corr_noise 0.5 --use_rgb
+
+
+
+
+# ### ALL RUNS
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 2 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 2 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 2 --folder ./results_new --exo_noise --video
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 20 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 20 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 20 --folder ./results_new --exo_noise --video
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new --exo_noise --video
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new --exo_noise --video
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_new --exo_noise --video
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_new --exo_noise --video
+
+
+
+
+
+
+
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 2 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 2 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 2 --folder ./results_new --exo_noise --video --use_rgb
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 20 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 20 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 20 --folder ./results_new --exo_noise --video --use_rgb
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 36 --folder ./results_new --exo_noise --video --use_rgb
+
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 40 --folder ./results_new --exo_noise --video --use_rgb
+
+
+# python train_rep.py --tag vq_genIK --walls empty --type genIK --use_vq --seed 1 --L_genik 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_cont --walls empty --type genIK --use_vq --seed 1 --L_coinv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_new --exo_noise --video --use_rgb
+# python train_rep.py --tag vq_inv --walls empty --type genIK --use_vq --seed 1 --L_inv 1.0 --use_logger --groups 1 --n_embed 80 --folder ./results_new --exo_noise --video --use_rgb
+
+
+
 
 
