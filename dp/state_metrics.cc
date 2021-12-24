97050151e6352e8ed2469abaf241c38581fa78e3
John Langford <jl@hunch.net>
2021-12-24 11:43:31 -0500

    Various optimizations and (apparently) a correctness improvement.

--- dp/state_metrics.cc
+++ dp/state_metrics.cc
@@ -6,10 +6,11 @@
 #include <math.h>
 #include <bits/stdc++.h>
 #include <gvc.h>
+#include <unordered_map>
 
 using namespace std;
 
-typedef vector<pair<string,float>> state_distribution;
+typedef unordered_map<string,float> state_distribution;
 
 const double epsilon = 1e-4;
 
@@ -28,10 +29,10 @@ void parse_state_distribution(char* line, state_distribution& sd)
     {
       char* probability = strtok(nullptr, " \t\n");
       if (probability != nullptr)
-	sd.push_back(make_pair(state,atof(probability)));
+	sd.emplace(state,atof(probability));
       else
-	sd.push_back(make_pair(state,1.));
-      total += sd.back().second;
+	sd.emplace(state,1.);
+      total += sd[state];
     }
   if (fabs(1. - total) > epsilon)
     {
@@ -61,56 +62,71 @@ void print_marginal(const marginal& m)
   cout << endl;
 }
 
-struct action_transition
+struct action_state
 {
   string action;
   string state;
-  state_distribution next_state_probability;
+
+  bool operator==(const action_state &other) const
+  { return (action == other.action
+            && state == other.state);
+  }
 };
 
-action_transition parse_action_transition(char* line)
+namespace std {
+template <>
+struct hash<action_state>
 {
-  action_transition ret;
-  ret.action = strtok(line, " \t");
-  ret.state = strtok(nullptr, " \t");
-  parse_state_distribution(line, ret.next_state_probability);
-  return ret;
+  size_t operator()(const action_state& k) const
+  {
+    return ((hash<string>()(k.action)
+	     ^ (hash<string>()(k.state) << 1)) >> 1);
+  }
+};
 }
 
-void print_action_transition(const action_transition& at)
+typedef unordered_map<action_state,state_distribution> action_transitions;
+
+void parse_action_transition(action_transitions& ats, char* line)
 {
-  cout << at.action << " " << at.state;
-  print_state_distribution(at.next_state_probability);
+  action_state as;
+  as.action = strtok(line, " \t");
+  as.state = strtok(nullptr, " \t");
+  state_distribution sd;
+  parse_state_distribution(line, sd);
+  ats.emplace(as,sd);
 }
 
-void print_action_transitions(const vector<action_transition>& ats)
+void print_action_transition(const pair<action_state,state_distribution>& at)
+{
+  cout << at.first.action << " " << at.first.state;
+  print_state_distribution(at.second);
+}
+
+void print_action_transitions(const action_transitions& ats)
 {
   for (auto& at : ats)
     print_action_transition(at);
   cout << endl;
 }
 
-struct state_explanation
-{
-  string state;
-  state_distribution alt_state_probability;
-};
+typedef unordered_map<string,state_distribution> state_explanations;
 
-state_explanation parse_state_explanation(char* line)
+void parse_state_explanation(state_explanations& se, char* line)
 {
-  state_explanation ret;
-  ret.state = strtok(line, " \t");
-  parse_state_distribution(line, ret.alt_state_probability);
-  return ret;
+  string key = strtok(line, " \t");
+  state_distribution sd;
+  parse_state_distribution(line, sd);
+  se.emplace(key,sd);
 }
 
-void print_state_explanation(const state_explanation& se)
+void print_state_explanation(const pair<string,state_distribution>& se)
 {
-  cout << se.state;
-  print_state_distribution(se.alt_state_probability);
+  cout << se.first;
+  print_state_distribution(se.second);
 }
 
-void print_state_explanations(const vector<state_explanation>& ses)
+void print_state_explanations(const state_explanations& ses)
 {
   for (auto& se : ses)
     print_state_explanation(se);
@@ -127,13 +143,13 @@ void min_entropy(const marginal& m)
   cout << "minimum entropy = " << min_value << endl;
 }
 
-double impurity(vector<state_explanation>& ses)
+double impurity(state_explanations& ses)
 {
   double total = 0.;
   for (auto& se : ses)
     {
       float max = 0.;
-      for (auto& sp : se.alt_state_probability)
+      for (auto& sp : se.second)
 	{
 	  if (sp.second > max)
 	    max = sp.second;
@@ -143,7 +159,7 @@ double impurity(vector<state_explanation>& ses)
   return total / ses.size();
 }
 
-void sss_and_dsm(vector<state_explanation>& learned_by_ground, vector<state_explanation>& ground_by_learned)
+void sss_and_dsm(state_explanations& learned_by_ground, state_explanations& ground_by_learned)
 { //sss = same_state_separated = uniform (over ground states) fraction not accounted for by a single learned state
   cout << "SSS (same state separated) = " << impurity(ground_by_learned) << endl;
 
@@ -156,33 +172,16 @@ bool most_probable_first(pair<string,float>& e1, pair<string,float>& e2)
   return e1.second > e2.second;
 }
 
-state_distribution find_ground_states(const string& state, const vector<state_explanation>& learned_by_ground)
-{
-  for (auto& se : learned_by_ground)//this could be constant instead of linear
-    if (se.state == state)
-      return se.alt_state_probability;
-  cerr << "badness! search for state '" << state << "' failed!" << endl;
-  state_distribution ret;
-  return ret;
-}
-
 void add_in(state_distribution& target, const state_distribution& source, float probability)
 {//could be more efficient with a sortable state.
   for(auto& ssp : source)
     {
-      bool not_added = true;
-      for(auto& tsp : target)
-	if (tsp.first == ssp.first)
-	  {
-	    tsp.second += ssp.second*probability;
-	    not_added = false;
-	    break;
-	  }
-      if (not_added)
-	{
-	  target.push_back(ssp);
-	  target.back().second *= probability;
-	}
+      float p = ssp.second * probability;
+      auto te = target.find(ssp.first);
+      if (te != target.end())
+	te->second += p;
+      else
+	target.emplace(ssp.first,p);
     }
 }
 
@@ -191,21 +190,25 @@ float difference(const state_distribution& sd1, const state_distribution& sd2)
   double total = 0.;
   for (auto& sp1 : sd1)
     {
-      bool not_diffed = true;
-      for (auto& sp2 : sd2)
-	if (sp1.first == sp2.first)
-	  {
-	    total += fabs(sp1.second - sp2.second);
-	    not_diffed = false;
-	    break;
-	  }
-      if (not_diffed)
+      auto sd2i = sd2.find(sp1.first);
+      if (sd2i != sd2.end())
+	total += fabs(sp1.second - sd2i->second);
+      else
 	total += sp1.second;
     }
   return total;
 }
 
-void transition_difference(vector<action_transition>& learned_transitions, vector<action_transition>& ground_transitions, vector<state_explanation>& learned_by_ground)
+void normalized(const state_distribution& sd)
+{
+  double total = 0.;
+  for (auto& sp : sd)
+    total+=sp.second;
+  if (fabs(total - 1.) > epsilon)
+    cerr << "normalization failure!" << endl;
+}
+
+void transition_difference(action_transitions& learned_transitions, action_transitions& ground_transitions, state_explanations& learned_by_ground)
 {
   //Every learned state induces a distribution over ground states via learned_by_ground.
   //For each learned state action, we have a distribution over learned next states.
@@ -218,28 +221,16 @@ void transition_difference(vector<action_transition>& learned_transitions, vecto
   for (auto& at : learned_transitions)
     {
       state_distribution p_learned_of_ground;
-      for (auto& sp : at.next_state_probability)
-	{// For each next learned state, find its distribution over ground states and add it in.
-	  state_distribution ground_state_distribution = find_ground_states(sp.first, learned_by_ground);
-	  add_in(p_learned_of_ground, ground_state_distribution, sp.second);
-	}
-      state_distribution ground_explanation_of_state = find_ground_states(at.state, learned_by_ground);
+      for (auto& sp : at.second) // For each next learned state, find its distribution over ground states and add it in.
+	add_in(p_learned_of_ground, learned_by_ground[sp.first], sp.second);
+      
+      state_distribution ground_explanation_of_state = learned_by_ground[at.first.state];
       
       state_distribution p_ground_of_ground;
-      for (auto& sp : ground_explanation_of_state)
+      for (auto& sp : ground_explanation_of_state) // For the state, apply its distribution over ground states to the next state distribution.
 	{
-	  bool not_added_in = true;
-	  for (auto& at2 : ground_transitions) // for each ground state, find matching next state distribution
-	    {
-	      if (sp.first == at2.state && at.action == at2.action)
-		{
-		  add_in(p_ground_of_ground, at2.next_state_probability, sp.second);
-		  not_added_in = false;
-		  break;
-		}
-	    }
-	  if (not_added_in)
-	    cout << "badness, can't find match for " << sp.first << endl;
+	  action_state as = {at.first.action, sp.first};
+	  add_in(p_ground_of_ground, ground_transitions[as], sp.second);
 	}
       total += difference(p_learned_of_ground, p_ground_of_ground);
     }
@@ -247,7 +238,7 @@ void transition_difference(vector<action_transition>& learned_transitions, vecto
   cout << "Dynamics difference = " << total / learned_transitions.size() << endl;
 }
 
-void graph_dynamics(vector<action_transition> learned_transitions)
+void graph_dynamics(action_transitions& learned_transitions)
 {//see here: https://graphviz.org/pdf/libguide.pdf for details
   Agraph_t *g;
   GVC_t *gvc;
@@ -262,19 +253,19 @@ void graph_dynamics(vector<action_transition> learned_transitions)
   vector<Agnode_t*> nodes;
   for (auto& ast : learned_transitions)
     {
-      Agnode_t* state_node = agnode(g,(char*)ast.state.c_str(),1);
+      Agnode_t* state_node = agnode(g,(char*)ast.first.state.c_str(),1);
       /* Set an attribute - in this case one that affects the visible rendering */
       agsafeset(state_node, (char*)"color", (char*)"red", (char*)"red");
       agsafeset(state_node, (char*)"fontsize", (char*)"8", (char*)"8");
       agsafeset(state_node, (char*)"height", (char*)"0.25", (char*)"0.25");
       agsafeset(state_node, (char*)"width", (char*)"0.25", (char*)"0.25");
-      string state_action = ast.state+"_"+ast.action;
+      string state_action = ast.first.state+"_"+ast.first.action;
       Agnode_t* state_action_node = agnode(g,(char*)state_action.c_str(),1);
       agsafeset(state_action_node, (char*)"color", (char*)"green", (char*)"");
       Agedge_t* e = agedge(g,state_node, state_action_node, 0, 1);
       agsafeset(e, (char*)"len", (char*)"0.5", (char*)"0.5");
       agsafeset(e, (char*)"style", (char*)"filled", (char*)"filled");
-      for (auto& sp : ast.next_state_probability)
+      for (auto& sp : ast.second)
 	{
 	  Agnode_t* next_state_node = agnode(g,(char*)sp.first.c_str(),1);
 	  Agedge_t* e = agedge(g,state_action_node,next_state_node,nullptr,1);
@@ -316,10 +307,10 @@ int main(int argc, char *argv[])
 
   marginal learned_marginals;
   marginal ground_marginals;
-  vector<action_transition> learned_transitions;
-  vector<action_transition> ground_transitions;
-  vector<state_explanation> learned_by_ground;
-  vector<state_explanation> ground_by_learned;
+  action_transitions learned_transitions;
+  action_transitions ground_transitions;
+  state_explanations learned_by_ground;
+  state_explanations ground_by_learned;
   
   size_t table = 0;
   bool mid_table = false;
@@ -345,19 +336,19 @@ int main(int argc, char *argv[])
 	learned_marginals = parse_marginal(line);
 	break;
       case 1: // learned state transition
-	learned_transitions.push_back(parse_action_transition(line));
+	parse_action_transition(learned_transitions,line);
 	break;
       case 2: // ground state occupancy
 	ground_marginals = parse_marginal(line);
 	break;
       case 3: // ground state transition
-	ground_transitions.push_back(parse_action_transition(line));
+	parse_action_transition(ground_transitions,line);
 	break;
       case 4: // learned state explanation
-	learned_by_ground.push_back(parse_state_explanation(line));
+	parse_state_explanation(learned_by_ground,line);
 	break;
       case 5: // ground state explanation
-	ground_by_learned.push_back(parse_state_explanation(line));
+	parse_state_explanation(ground_by_learned,line);
 	break;
       default:
 	cerr << "something wrong, unknown table" << endl;
